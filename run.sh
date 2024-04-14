2a0920adb1a52cf6774b25b90d6da3da53e46fb3
Jeff Irwin <jirwin505@gmail.com>
2024-04-14 12:32:14 -0400

    add options to run.sh and call it from github actions yml

--- run.sh
+++ run.sh
@@ -1,15 +1,43 @@
 #!/usr/bin/env bash
 
-set -xe
+#set -xe
+set -e
 
-if [[ $# -ge 1 ]] ; then
-	arg=$1
-else
-	arg="inputs/cubes.ribbit"
-fi
+#if [[ $# -ge 1 ]] ; then
+#	ribbit_file=$1
+#else
+#	ribbit_file="inputs/cubes.ribbit"
+#fi
 
-# Debug
-time fpm run --compiler ifx --c-compiler gcc --flag "-fpp -qmkl -heap-arrays0 -check noarg_temp_created" --profile debug -- "$arg"
+# Default arguments
+ribbit_file="inputs/cubes.ribbit"
+profile="debug"
 
-# TODO: release option
+pos=0
+while test $# -gt 0 ; do
+	#echo "$1"
+
+	# TODO: add --verbose option to pass along to fpm
+	if [[ "$1" == "--release" ]] ; then
+		profile="release"
+	elif [[ "$1" == "--debug" ]] ; then
+		profile="debug"
+
+	elif [[ "$pos" == "0" ]] ; then
+		ribbit_file="$1"
+		((++pos))
+		#echo "pos = $pos"
+
+	else
+		echo "Error: bad argument \"$1\""
+		exit -1
+	fi
+
+	shift
+done
+
+#echo "profile = $profile"
+#exit 0
+
+time fpm run --compiler ifx --c-compiler gcc --flag "-fpp -qmkl -heap-arrays0 -check noarg_temp_created" --profile $profile -- "$ribbit_file"
 
